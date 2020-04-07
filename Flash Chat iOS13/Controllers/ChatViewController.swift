472d8aa94aff9fceb9523b08bab5454ba8cd57eb
Kourtnie Henault <kourtnie.m.jenkins@exxonmobil.com>
2020-04-07 12:20:23 -0500

    Create a custom UITableViewCell for the chat view and a message struct to hold user chat data

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -13,13 +13,21 @@ class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+
+    private var mockMessages: [Message] = [
+        Message(sender: "User1", body: "Scelerisque fermentum dui faucibus in ornare quam viverra orci."),
+        Message(sender: "User2", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id. Id consectetur purus ut faucibus pulvinar elementum integer enim."),
+        Message(sender: "User3", body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.dataSource = self
         
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -35,5 +43,24 @@ class ChatViewController: UIViewController {
         }
         
     }
+    
+}
+
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mockMessages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        cell.label.text = mockMessages[indexPath.row].body
+        
+        return cell
+    }
+    
+ 
+  
     
 }
